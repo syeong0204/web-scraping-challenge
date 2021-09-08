5c9977e684bcb4479ecf2efc10d447c3a3f6fe2c
syeong0204 <syeong0204@gmail.com>
2021-09-08 00:44:26 -0400

    commented out code

--- scrape_mars.py
+++ scrape_mars.py
@@ -23,52 +23,66 @@ def scrape_info():
     soup = bs(html, 'html.parser')
     news_title = soup.find('div', class_='content_title').text
     news_p = soup.find('div', class_= 'article_teaser_body').text
-    # Store data in a dictionary
+    
     browser.quit()
 
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+  #  executable_path = {'executable_path': ChromeDriverManager().install()}
+   # browser = Browser('chrome', **executable_path, headless=False)
+
+# image scrape
 
 
-    url = 'https://spaceimages-mars.com/'
-    browser.visit(url)
+   # url = 'https://spaceimages-mars.com/'
+   # browser.visit(url)
 
-    time.sleep(1)
+   # time.sleep(1)
 
-    full_image = soup.find_all('a', class_= 'showimg fancybox-thumbs')['href']
-    featured_image_url = url + full_image
-    # Close the browser after scraping
-    browser.quit()
+   # full_image = soup.find_all('a', class_= 'showimg fancybox-thumbs')['href']
+   # featured_image_url = url + full_image
+   # # Close the browser after scraping
+   # browser.quit()
+#Hemisphere
 
+  #  url = 'https://marshemispheres.com/'
+  #  browser.visit(url)
+  #  hemi = []
 
-    hemi = []
-
-    for i in range(4):
-    html = browser.html
-    soup = bs(html, 'html.parser')
-
-    title = soup.find_all('h3')[i].get_text()
-    browser.find_by_tag('h3')[i].click()
+   # for i in range(8):
+   #     html = browser.html
+   #     soup = bs(html, 'html.parser')
+   # 'a', class_ = 'itemLink product-item'
+   #    title = soup.find_all('a', class_ = 'itemLink product-item')[i].get_text()
     
-    html = browser.html
-    soup = bs(html, 'html.parser')
+    #for i in range(4):
+    #    soup = bs(html, 'html.parser')
     
-    hemi_img = soup.find('a', text='Sample')['href']
-    browser.back()
-
-
-    hemi.append({
-        'title':title,
-        'hemi_img': hemi_img
-    })
+    #    browser.find_by_css("a.product-item h3")[i].click()
+    
+    #    html = browser.html
+     #   soup = bs(html, 'html.parser')
+      #  hemi_img = soup.find('a', text='Sample')['href']
+    #browser.back()
+    
+    #browser.find_by_text('Hemisphere Enhanced thumbnail')[i].click()
+    #browser.back()
+    #browser.find('img', class_ = 'thumb').click()
+    #hemi_img = soup.find('a', text='Sample')['href']
+  #  browser.back()
+   # browser.find_by_tag('h3')[i].click()
+   # hemi_full = soup.find('a', text = 'enhanced-full.jpg')
+    #if(len(title) >= 1):
+    #    {hemi.append({
+    #    'title':title.strip(),
+    #    'hemi_img': hemi_img
+    #})}
 
 
 
     mars_data = {
     "news_title": news_title,
     "news_p": news_p,
-    'featured_image_url': featured_image_url,
-    'hemi_scrape': hemi
+    #'featured_image_url': featured_image_url,
+    #'hemi_scrape': hemi
     }
     return mars_data
     
